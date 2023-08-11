c672ab29d7bca782fd4dd0f6f2745b461e081a48
lewisjn-arch <lhinga75@gmsil.com>
2023-08-11 14:17:25 -0700

    Edit whitespace

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of programme
+ *
+ * Description: This function pringle single digit base 10 numbers
+ *
+ * Return: Always returns 0 to mean test case passes
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 10)
+	{
+		num++;
+		putchar ('\n');
+	}
+	putchar ('\n');
+	return (0);
+}
