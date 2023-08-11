c4af7a549a89fe4a872e680eaadb8950670ed8d9
lewisjn-arch <lhinga75@gmsil.com>
2023-08-11 09:26:05 -0700

    Fix linter errors

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/* main = Starting point of code */
+/* Return: Always 0 (Means code is successful) */
 int main(void)
 {
 	int n;
@@ -11,11 +10,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	   if (n < 0)
-		 printf("%d is negative\n", n);
-	   else if (n > 0)
-		 printf("%d is positive\n", n);
-	   else 
+	if (n < 0)
+		printf("%d is negative\n", n);
+	else if (n > 0)
+		printf("%d is positive\n", n);
+	else
 		 printf("%d is zero\n", n);
 	return (0);
 }
