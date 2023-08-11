235c1581ce32df6d8d2a38af02242d37b5f0a38c
lewisjn-arch <lhinga75@gmsil.com>
2023-08-11 11:32:48 -0700

    Add last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,14 +6,21 @@
  *
  * Description: The function outputs the last digit and a string of status
  *
- * Returns: Always 0 (Success)  
+ * Return: Always 0 (Means the test case is a Success)
  */
 int main(void)
 {
 	int n;
+	int x;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	if (x < 6 && x != 0)
+		printf("Last digit of %d is %d  and is less than 6 and not 0\n", n, x);
+	else if (x > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, x);
+	else
+		printf("Last digit of %d is %d and is 0\n", n, x);
 	return (0);
 }
