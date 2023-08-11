0b694367d58df87ad3ca5a1182b3c80d4426f2b5
lewisjn-arch <lhinga75@gmsil.com>
2023-08-11 07:56:42 -0700

    modify positivity

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* main runs the code */
+/* more headers goes there */
 
 /* betty style doc for function main goes there */
 int main(void)
@@ -12,10 +12,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	   if (n < 0)
-		 printf("%d, is negative\n", n);
+		 printf("%d is negative\n", n);
 	   else if (n > 0)
-		 printf("%d, is positive\n", n);
+		 printf("%d is positive\n", n);
 	   else 
-		 printf("%d, is zero\n", n);
+		 printf("%d is zero\n", n);
 	return (0);
 }
