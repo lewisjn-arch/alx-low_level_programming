4d1e1fd22d713b816a499de29d90de91c0179596
lewisjn-arch <lhinga75@gmsil.com>
2023-08-14 08:59:19 -0700

    add double digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of programme
+ *
+ * Description: This function prints double digit numbers
+ *
+ * Return: Always returns 0 to mean the test case passes
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 100)
+	{
+		putchar (num + '0');
+		putchar (',');
+		putchar (' ');
+			if (num < 10)
+				putchar (num + "00");
+		num++;
+	}
+	putchar ('\n');
+	return (0);
+}
