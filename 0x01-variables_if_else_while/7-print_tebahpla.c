d69c629e67f6ea74e610399f5cba5c554afee511
lewisjn-arch <lhinga75@gmsil.com>
2023-08-14 02:36:27 -0700

    print reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of programme
+ *
+ * Description: This function prints the alphabet in reverse and lowecase
+ *
+ * Return: Always returns 0 to mean test case passes
+ */
+
+int main(void)
+{
+	char lower;
+
+	lower = 'z'
+	while (lower == 'z')
+	{
+		putchar (lower);
+		lower--;
+	}
+	putchar ('\n');
+	return (0);
+}
