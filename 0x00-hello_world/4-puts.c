3f768f61c310b01fba92cd276d2a75f5774d845a
lewisjn-arch <lhinga75@gmsil.com>
2023-08-14 11:21:57 -0700

    modify puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,15 @@
-puts("Programming is like building a multilingual puzzle");
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the programme
+ *
+ * Description: This function prints a string using puts function
+ *
+ * Return: Always returns 0 if the test case passes
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
