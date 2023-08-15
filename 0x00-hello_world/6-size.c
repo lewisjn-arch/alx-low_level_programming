09d37ac8591b016ec701491a4ccdbdceb9c1598c
lewisjn-arch <lhinga75@gmsil.com>
2023-08-15 02:57:16 -0700

    get data sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the programme
+ *
+ * Description: This programme prints the size of data types
+ *
+ * Return: Always returns 0 to mean test case passes
+ */
+
+int main(void)
+{
+	char c;
+	int num;
+	long int number'
+	long long int numbersLong;
+	float decimal;
+
+	printf ("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));	,
+	printf ("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(num));
+	printf ("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(number));
+	printf ("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(numbersLong));
+	printf ("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(decimal));
+	return (0);
+}
