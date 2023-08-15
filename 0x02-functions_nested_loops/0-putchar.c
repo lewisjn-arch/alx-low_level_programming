a43749d8cea0b0279b22ce8aa4a9b039e71bf5a2
lewisjn-arch <lhinga75@gmsil.com>
2023-08-15 13:09:01 -0700

    print to the standard output

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - This program prints to the standard output
+ *
+ * Description: This function prints a string of text
+ *
+ * Return: Always returns 0 to mean the test case passes
+ */
+
+int main (void)
+{
+	puts("_putchar");
+	return (0);
+}
