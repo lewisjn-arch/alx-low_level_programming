97e4e3f584f6b90e0a3c9c859233c7bab240031c
lewisjn-arch <lhinga75@gmsil.com>
2023-08-15 03:02:53 -0700

    modify sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,10 +17,10 @@ int main(void)
 	long long int numbersLong;
 	float decimal;
 
-	printf ("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));	,
-	printf ("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(num));
-	printf ("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(number));
-	printf ("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(numbersLong));
-	printf ("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(decimal));
+	printf ("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));	,
+	printf ("Size of int: %lu byte(s)\n", (unsigned long)sizeof(num));
+	printf ("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(number));
+	printf ("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(numbersLong));
+	printf ("Size of float: %lu byte(s)\n", (unsigned long)sizeof(decimal));
 	return (0);
 }
