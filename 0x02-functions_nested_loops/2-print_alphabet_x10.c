c4bc191df13741db66f3ae14531a378075caa28a
lewisjn-arch <lhinga75@gmsil.com>
2023-08-16 07:06:40 -0700

    repeat alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,26 @@
 #include "main.h"
 
 /**
- * main - This program contains nested loops
+ * print_alphabet_x10: This program prints the alphabet 10 times
  *
- * Description: This program prints the alphabet 10 times
- *
- * Always returns (0) to mean the test case passes
+ * Always returns void to mean the test case passes
  */
 
+void print_alphabet_x10(void);
+{
+	int repeat;
+	char lower;
 
+	repeat = 0;
+	while (repeat < 10)
+	{
+		lower = 'a';
+		while (lower <= 'z')
+		{
+			_putchar (lower);
+			lower++;
+		}
+	repeat++;
+	}
+	_putchar ('\n');
+}
