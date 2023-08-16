4c15b596c2411a78a5258d24e9722c7dff68ad8a
lewisjn-arch <lhinga75@gmsil.com>
2023-08-16 14:49:48 -0700

    modify

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** print_to_98 - this program prints numbers until 98
+ *
+ * @n: the value to be checked
+ *
+ * Return: 0 to mean success
+ */
+
+void print_to_98(int n)
+{
+	n = 0; 
+	while (n <= 98)
+	{
+		_putchar (n);
+		_putchar (',');
+		_putchar (' ');
+	l
+	}
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * jack_bauer - This program prints every minure of the day
+ *
+ * Return: returns ,if te test case passses
+ */
+
+void jack_bauer(void)
+{
+	int m, n, o, p;
+
+	for ()
+
+}
