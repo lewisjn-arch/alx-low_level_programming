8d32617a60b6b08c3fd07a27e2ed73eb10f3617c
lewisjn-arch <lhinga75@gmsil.com>
2023-08-17 11:06:13 -0700

    edit return valur

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,12 +6,12 @@
  *
  * @c: This is the value to be checked
  *
- * Return: Always 0
+ * Return: 1 if character is numerical and 0 if otherwise
  */
 
 int _isdigit(int c)
 {
-	if (c <= 48 && c >= 57)
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
