c5d6e6d334cf397aaa2ec8f2d51e680390c815d9
lewisjn-arch <lhinga75@gmsil.com>
2023-08-22 10:40:54 -0700

    reset value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this function updates the value of a variable
+ *
+ * @n: input
+ *
+ * Return: Always n
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is my first time learning pointers in c-programming lanuage
