df7e76d88ac8b175b985f39b760e475f7ea950dd
lewisjn-arch <lhinga75@gmsil.com>
2023-08-22 17:06:11 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts2 - prints characters in a string
+ *
+ * @str: string to be printed
+ *
+ * Return: print
+ */
+
+void puts2(char *str)
+{
+	int length = 0;
+	int m = 0;
+	char *k = str;
+	int n;
+
+	while (*y != '\0')
+	{
+		y++;
+		length;
+	}
+	m = length - 1;
+	for (n = 0; n <= m; n++)
+	{
+		if (n % 2 == 0)
+		{
+			_putchar(str[n]);
+		}
+		else
+		{
+			_putchar(str[n]);
+		}
+	}
+	_putchar('\n');
+}
