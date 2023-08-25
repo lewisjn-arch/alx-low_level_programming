f4df00468aa6792f1a422c76b9f097f3554b3b37
lewisjn-arch <lhinga75@gmsil.com>
2023-08-25 02:35:21 -0700

    replace header file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strcat - this function concatenates two strings
@@ -8,6 +7,7 @@
  *
  * Return: void
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int i;
