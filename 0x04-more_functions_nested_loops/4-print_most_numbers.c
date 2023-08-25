656e83fec191706a55dd33536db346144803c15b
lewisjn-arch <lhinga75@gmsil.com>
2023-08-25 06:43:43 -0700

    edit function parameters

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -14,8 +14,13 @@ void print_most_numbers(void)
 
 	for (c = 48; c < 58; c++)
 	{
-		if (c != 50 && c != 52)
-			_putchar(c);
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar(c);
+			}
+		}
 	}
 	_putchar('\n');
 }
