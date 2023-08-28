8b54596b4f7d188b6ad5ce9f5c290fd3be529896
lewisjn-arch <lhinga75@gmail.com>
2023-08-28 14:20:27 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - prints intergers in reverse order
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: void.
+ */
+
+void print_array(int *a, int n)
+{
+	int i, m;
+
+	for (i = 0; i < n--; i++)
+	{
+		m = a[i];
+		a[i] = a[n];
+		a[n] = m;
+	}
+}
