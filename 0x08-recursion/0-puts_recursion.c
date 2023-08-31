0017affa376c1b16cf275778e9a718ca25e3835f
lewisjn-arch <lhinga75@gmail.com>
2023-08-31 00:18:37 -0700

    print recursive string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - This recursive function prints a string
+ *
+ * @s: This is the string to be printed
+ * 
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[i]);
+		_puts_recursion(s);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This is my first time dealing wih a recursive function
