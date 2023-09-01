c879a78cd8693667e78900d9cb080b2e00ab8500
lewisjn-arch <lhinga75@gmail.com>
2023-09-01 08:53:36 -0700

    add functionality

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,6 +3,30 @@
 int calculate_sqrt(int n, int i);
 /**
  * calculate_sqrt - This function calculates the square root of a number
+ *
+ * @n: integer input
+ * @i: integer input
+ *
+ * Return: square root of a number
+ */
+
+int calculate_sqrt(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	else if (i * i == n)
+	{
+		return (i);
+	}
+	else
+	{
+		return (calculate_sqrt(n, i + 1));
+	}
+}
+
+/**
  * _sqrt_recursion - This function returns the square root of a number
  *
  * @n: integer input
@@ -10,34 +34,18 @@ int calculate_sqrt(int n, int i);
  * Return: square root of a number
  */
 
-int calculate_sqrt(int n, int i)
-{
-    if (i * i > n)
-    {
-        return (-1);
-    }
-    else if (i * i == n)
-    {
-        return (i);
-    }
-    else
-    {
-        return (calculate_sqrt(n, i + 1));
-    }
-}
-
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-    {
-        return (-1);
-    }
-    else if (n == 0 || n == 1)
-    {
-        return (n);
-    }
-    else
-    {
-        return (calculate_sqrt(n, 2));
-    }
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (calculate_sqrt(n, 2));
+	}
 }
