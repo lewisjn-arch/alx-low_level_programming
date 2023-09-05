7a5602e8542092276e46556608bcf58e29a43d7b
lewisjn-arch <lhinga75@gmail.com>
2023-09-05 16:02:32 -0700

    add strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - get ends of input and add together for size
+ *
+ * @s1: input one to concat
+ * @s2: input two to concat
+ *
+ * Return: concat of s1 and s2
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *sent;
+	int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+
+	sent = malloc(sizeof(char) * (i + j + 1));
+
+	if (sent == NULL)
+		return (NULL);
+
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		sent[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		sent[i] = s2[j];
+		i++, j++;
+	}
+	sent[i] = '\0';
+	return (sent);
+}
