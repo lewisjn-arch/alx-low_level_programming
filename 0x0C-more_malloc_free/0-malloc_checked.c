debeb58f52b93acc85baef0fc3cb0ad1e859eaa1
lewisjn-arch <lhinga75@gmail.com>
2023-09-07 12:56:29 -0700

    edit fucntion

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * malloc_checked - this function allocates memory using malloc function
  *
  * @b: the size of memory to be allocated
- *
- * Description: This function allocates memory as indicated on the argument
  *
  * Return: a pointer to the memory on heap memory
  */
