647af79f0f0aac80ce3fcc45c98c87b8589d741e
lewisjn-arch <lhinga75@gmail.com>
2023-09-20 03:19:10 -0700

    reduce code lines

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -41,14 +41,10 @@ char **strtow(char *str)
 
 	while (*(str + len))
 		len++;
-
 	words = count_word(str);
-
 	if (words == 0)
 		return (NULL);
-
 	matrix = (char **)malloc(sizeof(char *) * (words + 1));
-
 	if (matrix == NULL)
 		return (NULL);
 	for (i = 0; i <= len; i++)
@@ -58,14 +54,11 @@ char **strtow(char *str)
 			if (c)
 			{
 				end = i;
-
 				tmp = (char *)malloc(sizeof(char) * (c + 1));
-
 				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
 					*tmp++ = str[start++];
-
 				*tmp = '\0';
 				matrix[k] = tmp - c;
 				k++;
