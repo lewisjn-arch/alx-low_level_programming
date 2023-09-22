803bf83da7e78e2941207b178827c5cf9fee3283
lewisjn-arch <lhinga75@gmail.com>
2023-09-22 01:13:30 -0700

    edit betty errors

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,13 +9,13 @@
  */
 void free_list(list_t *head)
 {
-    list_t *t;
+	list_t *t;
 
-    while (head)
-    {
-        t = head->next;
-        free(head->str);
-        free(head);
-        head = t;
-    }
+	while (head)
+	{
+		t = head->next;
+		free(head->str);
+		free(head);
+		head = t;
+	}
 }
