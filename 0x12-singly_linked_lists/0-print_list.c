92fbe91c9d0c74dfd99e9c8710bb225f1f62355a
lewisjn-arch <lhinga75@gmail.com>
2023-09-22 01:25:52 -0700

    modify specifier

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,9 +17,9 @@ size_t print_list(const list_t *h)
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%lu] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		l++;
+		++l;
 	}
 	return (l);
 }
